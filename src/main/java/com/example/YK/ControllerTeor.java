310659dce47501b4e37749bd167dda7e6b7a41c5
YandereKiller <mrvlados254@gmail.com>
2024-03-11 13:40:41 +0400

    refactor

--- src/main/java/com/example/YK/ControllerTeor.java
+++ src/main/java/com/example/YK/ControllerTeor.java
@@ -13,9 +13,19 @@ public class ControllerTeor {
     @FXML
     protected void ont(ActionEvent event){ test1.setText("Test Successful");}
     @FXML
-    protected void onbackb(ActionEvent event){
+    protected void onbackb1(ActionEvent event){
         Application.root.getChildren().remove(Application.anc_teor.get(0));
         Application.root.getChildren().add(Application.mainscr.get(0));
+    }
+    @FXML
+    protected void onnextb1(ActionEvent event){
+        Application.root.getChildren().remove(Application.anc_teor.get(0));
+        Application.root.getChildren().add(Application.anc_teor.get(1));
+    }
+    @FXML
+    protected void onbackb2(ActionEvent event){
+        Application.root.getChildren().remove(Application.anc_teor.get(1));
+        Application.root.getChildren().add(Application.anc_teor.get(0));
     }
 
 }
