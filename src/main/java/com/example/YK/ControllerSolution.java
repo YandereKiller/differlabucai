164c982365e2b52b6e3b95ad2bd52b48e28e8dd9
YandereKiller <mrvlados254@gmail.com>
2024-03-22 20:05:13 +0400

    Начал работу над решением ЛР

--- src/main/java/com/example/YK/ControllerSolution.java
+++ src/main/java/com/example/YK/ControllerSolution.java
@@ -1,20 +1,58 @@
 package com.example.YK;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class ControllerSolution  {
+public class ControllerSolution implements Initializable {
+    static List<HBox> discrimsrt = new ArrayList<HBox>(){
+        {
+            try {
+                add((HBox) FXMLLoader.load(getClass().getResource("discrim1.fxml")));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    };
+    @FXML
+    protected ImageView task1img;
+    @FXML
+    protected HBox h;
+    @FXML
+    HBox b;
+
+    @FXML
+    protected ChoiceBox<String> discrim;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        discrim.getItems().addAll("D>0", "D=0", "D<0");
+        discrim.setOnAction(actionEvent -> {
+            switch(discrim.getValue()){
+                case "D>0":
+
+                    h.getChildren().clear();
+                    h.getChildren().add(discrimsrt.get(0));
+                case"D=0":
+                case"D<0":
+            }
+        });
+    }
 
 }
